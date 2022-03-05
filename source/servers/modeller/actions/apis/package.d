7b3a954998a2283f277f0f1515b2b517bcd11d4c
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-03-05 14:04:19 +0100

    Update

--- source/servers/modeller/actions/apis/package.d
+++ source/servers/modeller/actions/apis/package.d
@@ -5,20 +5,4 @@ import servers.modeller;
 import servers.modeller.settings.api;
 import models.modeller.entities.api;
 
-void uimApisCreateAction(HTTPServerRequest req, HTTPServerResponse res) {
-  debugMethodCall(moduleName!uimApisCreateAction~":uimApisCreateAction");
-
-  MDLAction_CreateApi(serverModeller).request(req, res);
-}
-
-void uimApisUpdateAction(HTTPServerRequest req, HTTPServerResponse res) {
-  debugMethodCall(moduleName!uimApisUpdateAction~":uimApisUpdateAction");
-
-  MDLAction_UpdateApi(serverModeller).request(req, res);
-}
-
-void uimApisDeleteAction(HTTPServerRequest req, HTTPServerResponse res) {
-  debugMethodCall(moduleName!uimApisDeleteAction~":uimApisDeleteAction");
-
-  MDLAction_DeleteApi(serverModeller).request(req, res);
-}
+mixin(ActionCalls!("uimApis", "MDLApi", "serverModeller"));
--- source/servers/modeller/actions/packages/package.d
+++ source/servers/modeller/actions/packages/package.d
@@ -8,17 +8,17 @@ import models.modeller.entities.model;
 void uimPackagesCreateAction(HTTPServerRequest req, HTTPServerResponse res) {
   debugMethodCall(moduleName!uimPackagesCreateAction~":uimPackagesCreateAction");
 
-  MDLAction_CreateModel(serverModeller).request(req, res);
+  MDLAction_CreatePackage(serverModeller).request(req, res);
 }
 
 void uimPackagesUpdateAction(HTTPServerRequest req, HTTPServerResponse res) {
   debugMethodCall(moduleName!uimPackagesUpdateAction~":uimPackagesUpdateAction");
 
-  MDLAction_UpdateModel(serverModeller).request(req, res);
+  MDLAction_UpdatePackage(serverModeller).request(req, res);
 }
 
 void uimPackagesDeleteAction(HTTPServerRequest req, HTTPServerResponse res) {
   debugMethodCall(moduleName!uimPackagesDeleteAction~":uimPackagesDeleteAction");
 
-  MDLAction_DeleteModel(serverModeller).request(req, res);
+  MDLAction_DeletePackage(serverModeller).request(req, res);
 }
